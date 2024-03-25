a73ae9a0a711dd8835735102ced1cf7f9bb8b9e6
cy-asus <cyang812@outlook.com>
2024-03-25 20:28:45 +0800

    add

--- deleteFile/deleteFile.py
+++ deleteFile/deleteFile.py
@@ -2,46 +2,79 @@
 # @Author: cyang
 # @Date:   2020-09-13 10:44:28
 # @Last Modified by:   cyang
-# @Last Modified time: 2020-09-19 15:47:58
+# @Last Modified time: 2023-11-25 15:28:56
 
 import os
 import send2trash
+from moviepy.editor import VideoFileClip
 
-SUFFIX = ('.torrent', '.txt', '.url', 'rar', '.jpg', '.mht', '.gif', '.png', '.mhtml', '.xltd', '.chm', '.bpm', '.apk', 'pdf')
-# SUFFIX = ('.torrent', '.txt', '.url', 'rar', '.mht','.mhtml', '.xltd', '.chm', '.ass')
-PATH = 'D:\迅雷下载'
+SUFFIX = ('.txt', '.jpg', '.rar')
+PATH = 'F:\download'
 
 def deleteFile(path):
-	for folderName, subFolders, fileNames in os.walk(path):
-		print('\n-----------------------------------')
-		print('The current folder is ' + folderName)
-		# print(subFolders, fileNames)
-		if not os.listdir(folderName):
-			print(folderName + ' is an empty folder, remove it')
-			filePath = os.path.join(folderName)
-			print('#send2trash#: ' + filePath)
-			send2trash.send2trash(filePath)
+    for folderName, subFolders, fileNames in os.walk(path):
+        print('\n-----------------------------------')
+        #print('The current folder is ' + folderName)
+        # print(subFolders, fileNames)
+        if not os.listdir(folderName):
+            #print(folderName + ' is an empty folder, remove it')
+            filePath = os.path.join(folderName)
+            delete(filePath, 'dir')
 
-		for subFolder in subFolders:
-			print('SUBFOLDER of ' + folderName + ': ' + subFolder)
-		for fileName in fileNames:
-			print('FILE INSIDE ' + folderName + ': ' + fileName)
+        #for subFolder in subFolders:
+            #print('SUBFOLDER of ' + folderName + ': ' + subFolder)
+        for fileName in fileNames:
+            # print('FILE INSIDE ' + folderName + ': ' + fileName)
 
-			#remove file to trash if its suffix is one of SUFFIX
-			if fileName.endswith(SUFFIX):
-				filePath = os.path.join(folderName, fileName)
-				print('#send2trash#: ' + filePath)
-				send2trash.send2trash(filePath)
-			elif fileName.endswith('.mp4'):
-				filePath = os.path.join(folderName, fileName)
-				fileSize = os.path.getsize(filePath)
-				fileSize = round(fileSize/float(1024*1024), 2)
-				print('%s, %d MB' % (filePath, fileSize))
+            #remove file to trash if its suffix is one of SUFFIX
+            if fileName.endswith(SUFFIX):
+                filePath = os.path.join(folderName, fileName)
+                delete(filePath, fileName.split('.')[-1])
+            elif fileName.endswith('.mp4'):
+                filePath = os.path.join(folderName, fileName)
+                fileSize = os.path.getsize(filePath)
+                fileSize = round(fileSize/float(1024*1024), 2)
+                #print('%s, %d MB' % (filePath, fileSize))
 
-				if fileSize < 100:
-					print('#send2trash#: ' + filePath)
-					send2trash.send2trash(filePath)
+                if fileSize > 20000000:
+                    delete(filePath, str(fileSize) + 'MB')
+                else:
+                    duration = getVideoInfo(filePath)
+                    if duration > 300000:
+                        delete(filePath, str(fileSize) + 'MB' + ' | ' + f"{duration:.2f}" + 's')
 
+def delete(filePath, type):
+    print('#send2trash# %15s | %s' % (type, filePath))
+    send2trash.send2trash(filePath)
+
+def getVideoInfo(file_path):
+    try:
+        # 打开视频文件
+        video_clip = VideoFileClip(file_path)
+
+        # 获取视频时长
+        duration = video_clip.duration
+
+        # 获取视频帧数
+        frames = video_clip.fps * duration
+
+        # 获取视频分辨率
+        resolution = video_clip.size
+
+        # 输出视频信息
+        print(f"视频时长: {duration} 秒")
+        # print(f"视频帧数: {frames} 帧")
+        # print(f"视频分辨率: {resolution[0]} x {resolution[1]} 像素")
+
+        return duration
+
+    except Exception as e:
+        print(f"发生错误: {str(e)}")
+
+    finally:
+        # 在 finally 块中调用 close() 方法确保资源的释放
+        if video_clip:
+            video_clip.close()
 
 if __name__ == '__main__':
-	deleteFile(PATH)
+    deleteFile(PATH)
---
+++ moveFile/moveFile.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+def copy_videos(source_folder, destination_folder, video_extensions=['.mp4', '.avi', '.mkv']):
+    # 遍历源文件夹中的所有文件和子文件夹
+    for root, dirs, files in os.walk(source_folder):
+        for file in files:
+            # 检查文件扩展名是否为视频扩展名
+            if any(file.lower().endswith(ext) for ext in video_extensions):
+                source_path = os.path.join(root, file)
+                # 构建目标路径
+                destination_path = os.path.join(destination_folder, file)
+                # 移动文件
+                shutil.move(source_path, destination_path)
+                # shutil.copy2(source_path, destination_path)
+                print(f"已移动：{source_path} 到 {destination_path}")
+
+# 替换以下路径为实际路径
+source_folder = 'F:\\download\\abc'
+destination_folder = 'F:\\1'
+
+copy_videos(source_folder, destination_folder)
